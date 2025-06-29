571f1bcd2fa3da19039b0b4399c4eff41b88f60c
Ruel Ryan A. Rosal <ruelryan@icloud.com>
2025-06-29 22:12:02 +0800

    Fix Font Registration Error in React PDF

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,22 @@ import { Font } from '@react-pdf/renderer';
 import App from './App.tsx';
 import './index.css';
 
+// Flag to prevent multiple font registrations
+let fontsRegistered = false;
+
 // Register PDF fonts once at application startup
-Font.register({
-  family: 'Roboto',
-  fonts: [
-    { src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-light-webfont.ttf', fontWeight: 300 },
-    { src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-regular-webfont.ttf', fontWeight: 400 },
-    { src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-medium-webfont.ttf', fontWeight: 500 },
-    { src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-bold-webfont.ttf', fontWeight: 700 },
-  ]
-});
+if (!fontsRegistered) {
+  Font.register({
+    family: 'Roboto',
+    fonts: [
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-light-webfont.ttf', fontWeight: 300 },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-regular-webfont.ttf', fontWeight: 400 },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-medium-webfont.ttf', fontWeight: 500 },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-bold-webfont.ttf', fontWeight: 700 },
+    ]
+  });
+  fontsRegistered = true;
+}
 
 // Only register service worker if supported (not in StackBlitz environment)
 if ('serviceWorker' in navigator && !window.location.hostname.includes('webcontainer')) {
